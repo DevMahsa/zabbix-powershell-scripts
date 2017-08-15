b7501075845796320af187b705c9e2c0f8dcd460
Mahsa Golchian <mahsa.gol89@gmail.com>
2017-08-15 08:30:13 +0430

    Update MSSQLFDLauncherstatus.ps1

--- MSSQLFDLauncherstatus.ps1
+++ MSSQLFDLauncherstatus.ps1
@@ -1,5 +1,5 @@
-﻿$s = (Get-Service | Where-Object {$_.Name -eq "MSSQLFDLauncher"} | Select-Object "status")
-if ($s -Match "running")
+﻿$mysqlfulltextlauncher = (Get-Service | Where-Object {$_.Name -eq "MSSQLFDLauncher"} | Select-Object "status")
+if ($mysqlfulltextlauncher -Match "running")
 {echo 0}
 else
 {echo 1}
