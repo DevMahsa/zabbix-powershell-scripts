bdba46b4b245b9831c43f12abbdcec9431ae6a0f
Mahsa Golchian <mahsa.gol89@gmail.com>
2017-08-15 08:32:37 +0430

    Update winactstat.ps1

--- winactstat.ps1
+++ winactstat.ps1
@@ -1,5 +1,5 @@
-$S= Get-WmiObject win32_OperatingSystem | Select-Object "SerialNumber"
-if ($S -Match "")
+$serialnumactivation= Get-WmiObject win32_OperatingSystem | Select-Object "SerialNumber"
+if ($serialnumactivation -Match "")
 {echo 1}
 else
 {echo 0}
