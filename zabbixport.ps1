b06d99b837dd11b5177f904158df799d833e634a
Mahsa Golchian <mahsa.gol89@gmail.com>
2017-08-15 08:34:00 +0430

    Update zabbixport.ps1

--- zabbixport.ps1
+++ zabbixport.ps1
@@ -1,5 +1,5 @@
-$S= netstat -nao | Select-String ":10050"
-if ($s -Match "Listening")
+$zabbixport= netstat -nao | Select-String ":10050"
+if ($zabbixport -Match "Listening")
 {echo 0}
 else
 {echo 1}
